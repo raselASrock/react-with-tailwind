3982d73a22e0f876cb30f5e89f4fe7436bab61ae
raselASrock <mdraselm413@gmail.com>
2022-10-04 00:47:40 +0600

    btn-interaction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <h1>This is heading h1.</h1>
       <p>This is normal paragraph text.</p>
     </div>
