434b1ed0919db18cea84700284f6521afc6c6877
raselASrock <mdraselm413@gmail.com>
2022-10-03 23:38:31 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is heading h1.</h1>
+      <p>This is normal paragraph text.</p>
+    </div>
+  );
+}
+
+export default App;
