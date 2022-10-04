6c9b2a3c20197e59105b3124b6d4ac2f9217c868
raselASrock <mdraselm413@gmail.com>
2022-10-04 20:28:47 +0600

    chart added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import AssignmentMarks from './Components/AssignmentMarks/AssignmentMarks';
 import NavBar from './Components/NavBar/NavBar';
 import Pricing from './Components/Pricing/Pricing';
 
@@ -10,6 +11,7 @@ function App() {
       <h1>This is heading h1.</h1>
       <p>This is normal paragraph text.</p>
       <Pricing></Pricing>
+      <AssignmentMarks></AssignmentMarks>
     </div>
   );
 }
