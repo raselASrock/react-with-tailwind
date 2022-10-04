3c53e49e99cf8f11d70cb9945e6f8e6377b828c5
raselASrock <mdraselm413@gmail.com>
2022-10-04 19:46:30 +0600

    responsive

--- src/Components/PriceOption/PriceOption.js
+++ src/Components/PriceOption/PriceOption.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { CheckCircleIcon } from '@heroicons/react/24/solid'
+import Feature from '../Feature/Feature';
+
 
 const PriceOption = ({option}) => {
+    const {features} = option;
     return (
         <div className="bg-indigo-300 m-6 rounded-md p-6">
             <div>
                 <p><span className='text-7xl  text-white font-bold'>{option.price}</span>
                 <span className='text-2xl text-gray-100'>/Month</span>
                 </p>
-                <h3 className='text-3xl font-semibold my-4'>{option.name}</h3>
+                <h3 className='text-3xl font-semibold my-4 text-yellow-400'>{option.name}</h3>
             </div>
-            <div className='flex items-center'>
-            <CheckCircleIcon className="h-6 w-6 text-blue-500"/>
-            <p className='ml-2'>Awesome Feature</p>
-            </div>
+            {
+                features.map( (feature, idx) => <Feature
+                key={idx} 
+                feature = {feature}
+                ></Feature>)
+            }
+            <button className='bg-blue-600 text-white text-2xl font-semibold p-2 mt-5 rounded-md w-full'>Buy Now</button>
         </div>
     );
 };
--- src/Components/Pricing/Pricing.js
+++ src/Components/Pricing/Pricing.js
@@ -13,7 +13,7 @@ const Pricing = () => {
         ]
     },
         {id:2, name: 'Medium', price: 2400, features: [
-            'Awesome Features',
+            'Every Thing on free',
             'Extra Service',
             "More Channel",
             "Ad Block Features",
@@ -21,7 +21,7 @@ const Pricing = () => {
             "Royal Feature"
         ]},
         {id:3, name: 'Premium', price: 3600, features: [
-            'Awesome Features',
+            'Every thing on premium',
             'Extra Service',
             "More Channel",
             "Ad Block Features",
@@ -32,7 +32,7 @@ const Pricing = () => {
     return (
         <div>
             <h2 className='text-5xl font-bold bg-indigo-300 p-12 text-white'>The best Deal.</h2>
-            <div className="grid grid-cols-3 gap-3">
+            <div className="grid md:grid-cols-3 gap-3">
             {
                 pricingOptions.map( option => <PriceOption
                 key={option.id}
