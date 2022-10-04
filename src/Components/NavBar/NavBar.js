ea4308377c023db3f78253e80069e05fd81dd271
raselASrock <mdraselm413@gmail.com>
2022-10-04 15:23:43 +0600

    nav toggle

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -6,19 +6,20 @@ const NavBar = () => {
     const [open,setOpen] = useState(false)
     const routes = [
         {id: 1, name: 'Home', path: '/home'},
-        {id: 2, name: 'Products', path: '/home'},
-        {id: 3, name: 'Orders', path: '/home'},
-        {id: 4, name: 'Contact', path: '/home'},
-        {id: 5, name: 'About', path: '/home'},
+        {id: 2, name: 'Products', path: '/products'},
+        {id: 3, name: 'Orders', path: '/orders'},
+        {id: 4, name: 'Contact', path: '/contact'},
+        {id: 5, name: 'About', path: '/about'},
     ]
     return (
-        <nav>
+        <nav className='bg-purple-500 w-full'>
+            <div onClick={() => setOpen(!open)} className="h-6 w-6 md:hidden">
             {
                 // open ? 'close-icon' : 'open-icon'
-                open ? <XMarkIcon onClick={() => setOpen(!open)} className="h-6 w-6"/>
-                 : <Bars3Icon onClick={() => setOpen(!open)} className="h-6 w-6"/>
+                open ? <XMarkIcon /> : <Bars3Icon/>
             }
-            <ul className='md:flex justify-center'>
+            </div>
+            <ul className={`bg-purple-400 md:flex w-full justify-center absolute md:static duration-500 ease-in ${open ? "top-6" : "top-[-120px]"}`}>
                 {
                     routes.map(route => <Link key={route.id} route = {route}></Link>)
                 }
